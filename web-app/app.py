7f46909346f43fcbee836fb4519db8a05505d55e
arostron <alex.rostron24@gmail.com>
2019-11-14 17:22:17 -0700

    Access private routes and make a png plot of all overlayed

--- web-app/app.py
+++ web-app/app.py
@@ -1,11 +1,14 @@
 from req_stuff import *
 from credential_handler import *
 from flask import Flask, redirect, request
+import matplotlib.pyplot as plt
+import polyline
+
 app = Flask(__name__)
 
 
 
-redir_url = "http://www.strava.com/oauth/authorize?client_id={}&response_type=code&redirect_uri=http://localhost:5000/exchange_token&approval_prompt=force&scope=read,activity:read".format(get_client_id())
+redir_url = "http://www.strava.com/oauth/authorize?client_id={}&response_type=code&redirect_uri=http://localhost:5000/exchange_token&approval_prompt=force&scope=read,activity:read_all".format(get_client_id())
 
 @app.route('/')
 def auth():
@@ -17,8 +20,14 @@ def get_token():
 	
 	code = request.args.get('code')
 	response = authenticate(get_client_id(), get_client_secret(), code)
-	return get_activities(response.json()['access_token']).content
-
+	activities = get_activities(response.json()['access_token']).json()
+	data = []
+	for a in activities:
+		for p in polyline.decode(a['map']['summary_polyline']):
+			data.append(p)
+	plt.scatter(*zip(*data))
+	plt.savefig("map.png")
+	return "Image saved"
 
 if __name__ == '__main__':
     app.run()
